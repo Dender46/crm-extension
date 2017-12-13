f29576c1f54b73adefb8fafab43404b4d09a2f30
Dmitriy Bagandov <dimon.bagandov@gmail.com>
2017-12-14 01:38:42 +0200

    Adapt to jquery for ease of dev

--- js/new-corp.js
+++ js/new-corp.js
@@ -1,28 +1,31 @@
 // ---------- CORPACK INFO ----------
-document.getElementById('corppack-source').value = 1;
-document.getElementById('corppack-source_comment').value = 'default_text';
-document.getElementById('corppack-mode_id').value = 15;
+$('#corppack-source').val(1);
+$('#corppack-source_comment').val('default_text');
+$('#corppack-mode_id').val(15);
 
 // ---------- DATE AND TIME ----------
-document.getElementById('corppack-time_start').value = '23:00';
-document.getElementById('corppack-time_end').value = '23:00';
-document.getElementById('corppack-prepare_start').value = '23:00';
+$('#corppack-time_start').val('23:00');
+$('#corppack-time_end').val('23:00');
+$('#corppack-prepare_start').val('23:00');
 
 // ---------- PREPAYMANT DETAILS ----------
-document.getElementById('corppack-percent').value = 0;
-var prepayment = document.getElementById('corppack-prepayment');
-prepayment.value = 0;
+$('#corppack-percent').val(0);
+var prepayment = $('#corppack-prepayment');
+prepayment.val(0);
 // on hover
-prepayment.onfocus = function() { prepayment.value = '' };
+prepayment.on("hover", function() { prepayment.val('') });
 
-var game = document.getElementById('corppack-game_price');
-var hide_and_seek = document.getElementById('corppack-hide_and_seek_price');
-var laser_tag = document.getElementById('corppack-laser_tag_price');
-var vr = document.getElementById('corppack-vr_room_price');
+var game = $('#corppack-game_price');
+var hide_and_seek = $('#corppack-hide_and_seek_price');
+var laser_tag = $('#corppack-laser_tag_price');
+var vr = $('#corppack-vr_room_price');
 
-game.value = hide_and_seek.value = laser_tag.value = vr.value = 0;
+game.val(0)
+hide_and_seek.val(0)
+laser_tag.val(0)
+vr.val(0);
 
-game.onfocus = function() { game.value = ''; };
-hide_and_seek.onclick = function() { hide_and_seek.value = ''; };
-laser_tag.onclick = function() { laser_tag.value = ''; };
-vr.onclick = function() { vr.value = ''; };
+game.on("focus", function() { game.val('') })
+hide_and_seek.on("focus", function() { hide_and_seek.val('') })
+laser_tag.on("focus", function() { laser_tag.val('') })
+vr.on("focus", function() { vr.val('') })
