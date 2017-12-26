d29a3a1366b28be22c28280c3eff4f7d20fd107a
Dmitriy Bagandov <dimon.bagandov@gmail.com>
2017-12-27 00:41:54 +0200

    Add semicolons at ends

--- js/games-search.js
+++ js/games-search.js
@@ -1,93 +1,93 @@
 $(document).ready(function() {
-  let date = new Date()
-  let day = date.getDate()
-  let month = date.getMonth()+1
-  let year = date.getFullYear()
+  let date = new Date();
+  let day = date.getDate();
+  let month = date.getMonth()+1;
+  let year = date.getFullYear();
 
   // to correct dates
   // ex. != 1.1.1970 but rather == 01.01.1970
   function setCorrectDate(day, month, year) {
-    let date
+    let date;
     if (day < 10)
       day = "0" + day;
     if (month < 10)
       month = "0" + month;
-    date = day + "." + month + "." + year
-    return date
+    date = day + "." + month + "." + year;
+    return date;
   }
 
   // SEARCH TODAY BUTTON
   $("#w0 fieldset:nth-of-type(1) .row:nth-of-type(4)").after(
-      "<button id='today-search' class='btn btn-primary'>Сегодня</button>")
+      "<button id='today-search' class='btn btn-primary'>Сегодня</button>");
   $("#today-search").click(function(event) {
-    event.preventDefault()
-    $("#w1").val(setCorrectDate(day, month, year))
-    $("#w2").val(setCorrectDate(day, month, year))
+    event.preventDefault();
+    $("#w1").val(setCorrectDate(day, month, year));
+    $("#w2").val(setCorrectDate(day, month, year));
   });
 
   // SEARCH DAY BEFORE
   // variables for day before
-  let beforeDay, beforeMonth, beforeYear
+  let beforeDay, beforeMonth, beforeYear;
   $("#today-search").before(
-    "<button id='before-day-search' class='btn btn-info'>-</button>")
+    "<button id='before-day-search' class='btn btn-info'>-</button>");
   $("#before-day-search").click(function(event) {
-    event.preventDefault()
+    event.preventDefault();
     if ($("#w1").val() == '')
-    return false
-    beforeDay = parseInt($("#w1").val().slice(0,2))
-    beforeMonth = parseInt($("#w1").val().slice(3,5))
-    beforeYear = parseInt($("#w1").val().slice(6,10))
+      return;
+    beforeDay = parseInt($("#w1").val().slice(0,2));
+    beforeMonth = parseInt($("#w1").val().slice(3,5));
+    beforeYear = parseInt($("#w1").val().slice(6,10));
     if (beforeDay > 1) {
-      beforeDay--
+      beforeDay--;
     } else {
-      beforeDay = 31
+      beforeDay = 31;
       if (beforeMonth > 1) {
-        beforeMonth--
+        beforeMonth--;
       } else {
-        beforeMonth = 12
-        beforeYear--
+        beforeMonth = 12;
+        beforeYear--;
       }
     }
-    $("#w1").val(setCorrectDate(beforeDay, beforeMonth, beforeYear))
-    $("#w2").val(setCorrectDate(beforeDay, beforeMonth, beforeYear))
-  })
+    $("#w1").val(setCorrectDate(beforeDay, beforeMonth, beforeYear));
+    $("#w2").val(setCorrectDate(beforeDay, beforeMonth, beforeYear));
+  });
 
   // SEARCH NEXT DAY BUTTON
   // variables for the next date
-  let nextDay, nextMonth, nextYear
+  let nextDay, nextMonth, nextYear;
 
-  $("#today-search").after("<button id='next-day-search' class='btn btn-info'>+</button>")
+  $("#today-search").after("<button id='next-day-search' class='btn btn-info'>+</button>");
   $("#next-day-search").click(function(event) {
-    event.preventDefault()
+    event.preventDefault();
     if ($("#w1").val() == '')
-      return false
-    nextDay = parseInt($("#w1").val().slice(0,2))
-    nextMonth = parseInt($("#w1").val().slice(3,5))
-    nextYear = parseInt($("#w1").val().slice(6,10))
+      return;
+    nextDay = parseInt($("#w1").val().slice(0,2));
+    nextMonth = parseInt($("#w1").val().slice(3,5));
+    nextYear = parseInt($("#w1").val().slice(6,10));
     if (nextDay < 31) {
       nextDay++;
     } else {
-      nextDay = 1
+      nextDay = 1;
       if (nextMonth < 12) {
-        nextMonth++
+        nextMonth++;
       } else {
-        nextMonth = 1
-        nextYear++
+        nextMonth = 1;
+        nextYear++;
       }
     }
-    $("#w1").val(setCorrectDate(nextDay, nextMonth, nextYear))
-    $("#w2").val(setCorrectDate(nextDay, nextMonth, nextYear))
+    $("#w1").val(setCorrectDate(nextDay, nextMonth, nextYear));
+    $("#w2").val(setCorrectDate(nextDay, nextMonth, nextYear));
   });
 
   // styles for button
-  $("#today-search").css("padding", "5px 8px")
-  $("#next-day-search, #before-day-search").css("padding", "0px 20px")
-  $("#next-day-search, #before-day-search").css("font-size", "1.85rem")
-  $("#today-search, #next-day-search, #before-day-search").css("margin", "0 8px 8px 0")
+  $("#today-search").css("padding", "5px 8px");
+  $("#next-day-search, #before-day-search").css("padding", "0px 20px");
+  $("#next-day-search, #before-day-search").css("font-size", "1.85rem");
+  $("#today-search, #next-day-search, #before-day-search").css("margin", "0 8px 8px 0");
 
   // SELECT DEFAULT OPTIONS FOR GAMES STATUSES
-  $("#gamesearch-game_status_id").attr("size", "6")
-  $("#gamesearch-game_status_id option[value=3]").attr("selected", "true")
-  $("#gamesearch-game_status_id option[value=5]").attr("selected", "true")
+  $("#gamesearch-game_status_id").attr("size", "6");
+  $("#gamesearch-game_status_id option[value=3]").attr("selected", "true");
+  $("#gamesearch-game_status_id option[value=5]").attr("selected", "true");
 
-})
+});
