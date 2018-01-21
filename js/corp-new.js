190472f8817b391673f6e190137e7e42ad53277d
Dmitriy Bagandov <dimon.bagandov@gmail.com>
2018-01-21 19:17:32 +0200

    Refactored code

--- js/corp-new.js
+++ js/corp-new.js
@@ -10,9 +10,9 @@ $('#corppack-prepare_start').val('23:00');
 
 // PREPAYMANT DETAILS
 // hiding corppack status unnecesary options
-$('#corppack-status_id option:nth-of-type(1)').hide()
-$('#corppack-status_id option[value=\'7\']').hide()
-$('#corppack-status_id option[value=\'10\']').hide()
+$('#corppack-status_id option:nth-of-type(1)').hide();
+$('#corppack-status_id option[value=\'7\']').hide();
+$('#corppack-status_id option[value=\'10\']').hide();
 
 $('#corppack-percent').val(0);
 var prepayment = $('#corppack-prepayment');
@@ -20,7 +20,7 @@ prepayment.val(0);
 prepayment.on('focus', function() { prepayment.val('') });
 prepayment.on('blur', function() {
   if (prepayment.val() > 0)
-    $('#corppack-status_id').val(2)
+    $('#corppack-status_id').val(2);
 });
 
 // CORPPACK PRICES
@@ -29,40 +29,40 @@ var hide_and_seek = $('#corppack-hide_and_seek_price');
 var laser_tag = $('#corppack-laser_tag_price');
 var vr = $('#corppack-vr_room_price');
 
-game.val(0)
-hide_and_seek.val(0)
-laser_tag.val(0)
+game.val(0);
+hide_and_seek.val(0);
+laser_tag.val(0);
 vr.val(0);
 
 game.on('focus', function() {
   if (game.val() == 0)
     game.val('')
-})
+});
 hide_and_seek.on('focus', function() {
   if (hide_and_seek.val() == 0)
     hide_and_seek.val('')
-})
+});
 laser_tag.on('focus', function() {
   if (laser_tag.val() == 0)
     laser_tag.val('')
-})
+});
 vr.on('focus', function() {
   if (vr.val() == 0)
     vr.val('')
-})
+});
 
 // some styling to pack prices on one Row and making it look pretty
 $('div.form-group.field-corppack-game_price.required >' +
-    'section').removeClass('col-4').addClass('col-3')
+    'section').removeClass('col-4').addClass('col-3');
 $('div.form-group.field-corppack-hide_and_seek_price.required >' +
-    'section').removeClass('col-4').addClass('col-3')
+    'section').removeClass('col-4').addClass('col-3');
 $('div.form-group.field-corppack-laser_tag_price >' +
-    'section').removeClass('col-4').addClass('col-3')
+    'section').removeClass('col-4').addClass('col-3');
 $('div.form-group.field-corppack-vr_room_price >' +
-    'section').removeClass('col-4').addClass('col-3')
-$('fieldset:nth-child(7) > div:nth-child(7) > fieldset').css('padding-bottom', '55px')
+    'section').removeClass('col-4').addClass('col-3');
+$('fieldset:nth-child(7) > div:nth-child(7) > fieldset').css('padding-bottom', '55px');
 
 // Moving Date section to Player selection row to reduce space usage
-let dateSection = $('fieldset:nth-child(5) > div > div.form-group.field-corppack-date.required > section')
-let playerRow = $('fieldset:nth-child(3) > div:nth-child(5)')
-dateSection.prependTo(playerRow)
+let dateSection = $('fieldset:nth-child(5) > div > div.form-group.field-corppack-date.required > section');
+let playerRow   = $('fieldset:nth-child(3) > div:nth-child(5)');
+dateSection.prependTo(playerRow);
