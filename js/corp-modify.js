ef59128ac8f64d1e3ad19f8a44a823357335aacb
Dmitriy Bagandov <dimon.bagandov@gmail.com>
2018-02-16 12:09:33 +0200

    Add and modify js file for corppack-update page

--- js/corp-modify.js
+++ js/corp-modify.js
@@ -14,13 +14,12 @@ $('#corppack-status_id option:nth-of-type(1)').hide();
 $('#corppack-status_id option[value=\'7\']').hide();
 $('#corppack-status_id option[value=\'10\']').hide();
 
-$('#corppack-percent').val(0);
 var prepayment = $('#corppack-prepayment');
-prepayment.val(0);
-prepayment.on('focus', function() { prepayment.val('') });
+var corppackStatus = $('#corppack-status_id');
+prepayment.on('focus', handleInput);
 prepayment.on('blur', function() {
-  if (prepayment.val() > 0)
-    $('#corppack-status_id').val(2);
+  if ($(this).val() > 0 && corppackStatus.val() != 8)
+    corppackStatus.val(2);
 });
 
 // CORPPACK PRICES
@@ -28,11 +27,6 @@ var game = $('#corppack-game_price');
 var hide_and_seek = $('#corppack-hide_and_seek_price');
 var laser_tag = $('#corppack-laser_tag_price');
 var vr = $('#corppack-vr_room_price');
-
-game.val(0);
-hide_and_seek.val(0);
-laser_tag.val(0);
-vr.val(0);
 
 function handleInput(e) {
   if (e.type == 'focus') {
